7cab64516df1f4ccf4dc1a21caf06b46139cde36
mod create <86007473+mine-fert228@users.noreply.github.com>
2023-02-16 14:00:19 +0700

    Add files via upload

---
+++ cmdm-linx on win.py
@@ -0,0 +1,135 @@
+import random
+import time
+import webbrowser
+import os
+import getpass
+import requests
+from pytube import YouTube
+
+from colorama import Fore, Style , Back
+
+USER_NAME = getpass.getuser()
+
+os.system('cls')
+
+name = getpass.getuser()
+
+
+a = os.path.basename(__file__)
+b = os.path.abspath(__file__).replace(a, '')
+
+print(Fore.RED + "версия r1.4-linux on windows")
+print(Fore.GREEN + "если нужна помощь ведите help"+ Style.RESET_ALL)
+i = 10
+for i in range(1,100):
+    
+    print(Fore.BLUE + name+":",b)
+    comand = input(Fore.GREEN + "$ " + Style.RESET_ALL)
+
+    if "close" in comand:
+        break
+
+    elif "clear" in comand:
+        os.system('cls')
+
+    elif comand == "rename":
+        name = input("Ведите новое имя: ")
+            
+
+        
+    
+
+    elif "help" in comand:
+        print("random - генератор числа от нуля до квадриллиона")
+        print("rename - переимевать ваше имя")
+        print('help - подксазка')
+        print("import - импортирование компонентов")
+        print("close - закрыть кмд")
+        print("calc - калькулятор")
+        print("youtube - скачать видео с youtube")
+        print("echo - спамит в терменал")
+        print('clear - очистка терминала')
+        print("cmd - команда от cmd linux")
+        print("pack install - установка приложений на ваш пк")
+
+
+    elif "random" in comand:
+        rend = random.randint(0,1000000000000000)
+        print(rend)
+        
+
+
+    
+    elif "calc" in comand:
+      print(eval(input("ведите пример: ")))
+    
+    elif "echo" in comand:
+        ioa = input("enter text: ")
+        sd = 50
+        for y in range(1, sd+1):
+            time.sleep(0.1)
+            print(ioa)
+
+
+
+    elif "youtube" in comand:
+            
+
+            yt = YouTube(input("ведите ссылку на видео (youtube): "))
+            stream = yt.streams.get_by_itag(22) 
+            stream.download()
+
+
+    
+    elif "cmd" in comand:
+        cmds = input("команда: ")
+        os.system(cmds)
+
+    elif "browser" in comand:
+        web = input("ссылка на сайт: ")
+        webbrowser.open_new_tab(web)
+    # импорт
+
+
+
+    elif "import" in comand:
+        print("доступные пакеты: python")
+        input()
+        
+    elif comand == "import python":
+        h = 1
+        for h in range(1, 101):
+            time.sleep(0.1)
+            os.system('cls')
+            print(h)
+            h+1
+        os.system('cls')
+        os.system('python')
+        print("загрузка завершена")
+
+    elif "pack install" in comand:
+        def save_from_site(link,namefile):
+            r = requests.get(link, allow_redirects=True)
+            open(namefile, "wb").write(r.content)
+            print(f"загрузка завершена Путь: {os.getcwd()}\{namefile}")
+        print('доступные пакеты:')
+        print('blot - блокнот')
+        print('my pyw - мой файл')
+        print('my py - мой файл')
+        pack = input('Название пакета: ')
+        if pack == 'my pyw':
+            ans = "имя файла: "
+            anb = 'ссылка(только с .pyw): '
+            save_from_site(input(anb),f"{input(ans)}.pyw")
+        elif pack == 'my py':
+            ans = "имя файла: "
+            anb = 'ссылка(только с .py): '
+            save_from_site(input(anb),f"{input(ans)}.py")
+        elif pack == 'blot':
+            save_from_site('https://raw.githubusercontent.com/mine-fert228/dhdonlineshop/main/ea3a5b32bf2688fd.uto','блокнот.pyw')
+        else:
+            print(Fore.RED + f"Нет пакета по имени {pack}")
+
+    else:
+        print(Fore.RED + "'",comand,"'",'не является внутренней или внешней')
+        print('командой, исполняемой программой или пакетным файлом.' + Style.RESET_ALL)
